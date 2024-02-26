0f9dba33d64ecde4b434c70e37eadcd6eedbf007
권신학 <cheatkey.dev@gmail.com>
2024-02-26 22:53:29 +0900

    github corner 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import MermaidRenderer from "./components/mermaidRenderer/mermaidRenderer";
 import TextareaAutosize from "react-textarea-autosize";
+import GithubCorner from "./components/GithubCorner/GithubCorner";
 
 const App = () => {
   const [mermaidString, setMermaidString] = useState(
@@ -13,26 +14,29 @@ const App = () => {
   );
 
   return (
-    <div className="w-full min-h-screen bg-gray-100 flex justify-center">
-      <main className="px-8 py-16 flex flex-col gap-6 w-full max-w-[1200px]">
-        <h1
-          className="text-transparent bg-clip-text text-5xl font-extrabold w-fit"
-          style={{
-            backgroundImage: `linear-gradient(90deg,#fb63a9,#68bde2)`,
-          }}
-        >
-          Beautiful Mermaid
-        </h1>
+    <>
+      <GithubCorner />
+      <div className="w-full min-h-screen bg-gray-100 flex justify-center">
+        <main className="px-8 py-16 flex flex-col gap-6 w-full max-w-[1200px]">
+          <h1
+            className="text-transparent bg-clip-text text-5xl font-extrabold w-fit"
+            style={{
+              backgroundImage: `linear-gradient(90deg,#0093E9,#80D0C7)`,
+            }}
+          >
+            Beautiful Mermaid
+          </h1>
 
-        <TextareaAutosize
-          className="bg-dark-700 text-dark-200 p-4 rounded-xl leading-6"
-          value={mermaidString}
-          onChange={(e) => setMermaidString(e.target.value)}
-        />
+          <TextareaAutosize
+            className="bg-dark-700 text-dark-200 p-4 rounded-xl leading-6"
+            value={mermaidString}
+            onChange={(e) => setMermaidString(e.target.value)}
+          />
 
-        <MermaidRenderer mermaidString={mermaidString} />
-      </main>
-    </div>
+          <MermaidRenderer mermaidString={mermaidString} />
+        </main>
+      </div>
+    </>
   );
 };
 
